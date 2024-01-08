572b32c7e7449b8bf18d70c826d3b7dfc2d96fbf
RaodaORI <115109599+RaodaORI@users.noreply.github.com>
2024-01-08 18:06:15 +0200

    Add files via upload

---
+++ coffee shop/js/js.js
@@ -0,0 +1,9 @@
+let loader = document.querySelector(".loadere");
+
+// if (loader.style.opacity == 0) {
+//   loader.style.display = "none";
+// }
+
+setInterval(() => {
+  loader.style.display = "none";
+}, 7000);
